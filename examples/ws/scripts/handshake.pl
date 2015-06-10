5cb34979e4138c40e5413bf4130370dd6ab6563d
Matthew Balch <matbalch@vt.edu>
2015-06-10 14:02:01 -0400

    Fixed handshake.pl in websocket example

--- examples/ws/scripts/handshake.pl
+++ examples/ws/scripts/handshake.pl
@@ -17,8 +17,9 @@ my %fields;
 # Read the fields
 while(<$sock_in>) {
   chomp;
-  if($_)
+  if($_) {
     last;
+  }
   ($key, $value) = split(':', $_, 2);
   # remove leading whitespace from value
   $value =~ s/^\s+//;
@@ -33,23 +34,23 @@ while(<$sock_in>) {
 if($method ne "GET") {
   # Needs to fail for non-get requests
   # Send a 405 "method not allowed" error with GET in the Allow header field
-  print($sock_out, "HTTP/1.1 405 method not allowed\r\n");
-  print($sock_out, "Allow:GET\r\n\r\n");
+  print {$sock_out} "HTTP/1.1 405 method not allowed\r\n";
+  print {$sock_out} "Allow:GET\r\n\r\n";
   # Exit with 1 to indicate failure
   exit 1;
 }
 # Check Upgrade tag
 if($fields{"upgrade"} ne "") {
   # Send a 426 "Upgrade Required" error
-  print($sock_out, "HTTP/1.1 426 upgrade required\r\n");
-  print($sock_out, "Upgrade:websocket\r\n\r\n");
+  print {$sock_out} "HTTP/1.1 426 upgrade required\r\n";
+  print {$sock_out} "Upgrade:websocket\r\n\r\n";
   exit 1;
 }
   
 # Check Connection tag case insensitively
 if(lc($fields{"connection"}) ne "upgrade") {
   # Give a more general 400 response
-  print($sock_out, "HTTP/1.1 400 bad request\r\n\r\n");
+  print {$sock_out} "HTTP/1.1 400 bad request\r\n\r\n";
   exit 1;
 }
 
@@ -60,18 +61,18 @@ if(lc($fields{"connection"}) ne "upgrade") {
 # Sec-WebSocket-Version must contain 13
 # if it fails, send out the acceptable version
 if($fields{"sec-websocket-version"}) {
-  print($sock_out, "HTTP/1.1 400 bad request\r\n");
-  print($sock_out, "Sec-WebSocket-Version:13\r\n\r\n");
+  print {$sock_out} "HTTP/1.1 400 bad request\r\n";
+  print {$sock_out} "Sec-WebSocket-Version:13\r\n\r\n";
   exit 1;
 }
 
 # Check Sec-WebSocket-Key for generation of Sec-WebSocket-Accept
 
 
-print($sock_out, "HTTP/1.1 101 switching protocols\r\n");
-print($sock_out, "Upgrade:websocket\r\n");
-print($sock_out, "Connection:Upgrade\r\n");
-print($sock_out, "Sec-WebSocket-Accept:\r\n\r\n");
+print {$sock_out} "HTTP/1.1 101 switching protocols\r\n";
+print {$sock_out} "Upgrade:websocket\r\n";
+print {$sock_out} "Connection:Upgrade\r\n";
+print {$sock_out} "Sec-WebSocket-Accept:\r\n\r\n";
 
 # Pass everything onto the next process
 
